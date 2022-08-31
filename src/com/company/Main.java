f8ec09c719e02fcc3328b604094781291ebaf404
shihabfaruk <sfaruk3131p@gmail.com>
2022-08-31 17:50:33 -0400

    includes selection sort descending order.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int[] arr = {2, 8, 5, 3, 9, 4, 1};
-        selectionSort(arr);
+        selectSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
+    //sorts in ascending
     public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length-1; i++) {
             int minInd = i;
@@ -22,6 +23,22 @@ public class Main {
 
             int temp = arr[minInd];
             arr[minInd] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    //sorts in descending
+    public static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            int maxInd = i;
+            for (int j = i +1; j < arr.length; j++) {
+                if (arr[j] > arr[maxInd]) {
+                    maxInd = j;
+                }
+            }
+
+            int temp = arr[maxInd];
+            arr[maxInd] = arr[i];
             arr[i] = temp;
         }
     }
